9923ca9f23b4e5dd34bc33e92b923f9fe0655a90
TripleMap <trippal1990@gmail.com>
2017-12-05 18:16:37 +0300

    slide

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,23 +1,30 @@
-import { Component, ChangeDetectorRef, AfterViewInit } from '@angular/core';
-import { MatIconRegistry } from '@angular/material';
-import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, ChangeDetectorRef, AfterViewInit } from "@angular/core";
+import { MatIconRegistry } from "@angular/material";
+import { MediaChange, ObservableMedia } from "@angular/flex-layout";
+
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements AfterViewInit {
-	name = 'Фильтрация';
-	single: any[];
-	multi: any[];
-	miniNav: boolean = false;
+	public activeMediaQuery = "";
+	public isActive:boolean = false;
 
-	private _mobileQueryListener: () => void;
-	constructor(public iconRegistry: MatIconRegistry, public changeDetectorRef: ChangeDetectorRef) {
-		this.iconRegistry.registerFontClassAlias('materialdesignicons', 'mdi');
+	constructor(
+		public iconRegistry: MatIconRegistry,
+		public changeDetectorRef: ChangeDetectorRef,
+		public media: ObservableMedia
+	) {
+		this.iconRegistry.registerFontClassAlias("materialdesignicons", "mdi");
+		media.subscribe((change: MediaChange) => this.activeMediaQuery = change ? change.mqAlias : "");
 	}
 
 	ngAfterViewInit(): void {
 		this.changeDetectorRef.detectChanges();
+	}
+
+	toggle(): void {
+		this.isActive = !this.isActive;
 	}
 }
