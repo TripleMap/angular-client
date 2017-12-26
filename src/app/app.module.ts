e86012981575f09020c2259efe9256d93ab21da9
TripleMap <trippal1990@gmail.com>
2017-12-26 23:11:38 +0300

    tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,11 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 // materialComponents
 import {
-  MatToolbarModule,
   MatIconRegistry,
   MatTooltipModule
 } from "@angular/material";
 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { FlexLayoutModule } from "@angular/flex-layout";
@@ -33,6 +33,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatSelectModule } from "@angular/material/select";
 import { MatCardModule } from "@angular/material/card";
+import { MatTabsModule } from '@angular/material/tabs';
 
 // services
 import { ApiHTTPInterceptorService } from "../services/ApiHTTPInterceptorService";
@@ -92,6 +93,7 @@ import { CovalentVirtualScrollModule } from "@covalent/core";
     MatFormFieldModule,
     MatSelectModule,
     MatCardModule,
+    MatTabsModule,
     CovalentVirtualScrollModule
   ],
   providers: [
