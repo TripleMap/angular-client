738e5ff923c39d25fee8b4d1d08e3d8338e1b9fe
TripleMap <trippal1990@gmail.com>
2018-01-09 23:21:38 +0300

    some tdm update

--- src/components/td-map-panel/td-map-panel.component.ts
+++ src/components/td-map-panel/td-map-panel.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit, Output, EventEmitter} from "@angular/core";
 import { OverLaysService } from "../../services/OverLaysService";
-
+import { TdDataTableService, TdDataTableSortingOrder, ITdDataTableSortChangeEvent, ITdDataTableColumn } from '@covalent/core'
 @Component({
 	selector: "td-map-panel",
 	templateUrl: "./td-map-panel.component.html",
@@ -12,21 +12,35 @@ export class TdMapPanelComponent implements AfterViewInit {
 	public multiple: boolean = true;
 	public sortBy: string = 'id';
 	public visibleFeatures: any[];
-
+	public pageSize: number = 20;
+	public columns: any[] = this.getColumnNames();
+	public filteredTotal: number = this.visibleFeatures ? this.visibleFeatures.length ?  this.visibleFeatures.length : 0:0;
+	public sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
 	@Output()
   	closeTdmapPanelSidenav: EventEmitter<string> = new EventEmitter<string>();
 
 	constructor(public _overLaysService: OverLaysService) {
+
 	}
 
 	ngAfterViewInit() {
 		this._overLaysService.mainLayer.featuresFlow.subscribe(data =>{
-			this.visibleFeatures = data;
+			this.visibleFeatures = data.concat([...data,...data,...data,...data,...data,...data,...data,...data,...data,...data,...data,...data]);
 			console.log(data)
 		});
 	}
 
 	toggleSideNav() {
     	this.closeTdmapPanelSidenav.emit('close-tdmap-panel-sidenav');
+  	}
+
+  	getColumnNames(){
+  		return [{
+  			 name: 'type',  label: 'Тип'
+  		}, {
+  			 name: 'geometry',  label: 'Геометрия'
+  		}, {
+  			 name: 'properties.id',  label: 'ID'
+  		} ]
   	}
 }
